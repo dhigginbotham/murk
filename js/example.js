0883414d103455c25f31f2ea4ab4a92e586862dc
David Higginbotham <davehigginbotham@gmail.com>
2015-07-04 04:01:50 -0600

    Update 2015-07-04T10:01:48.154Z

--- js/example.js
+++ js/example.js
@@ -1,29 +1,52 @@
-$(function() {
+(function(w,d) {
+  var m, modelOutput;
+
+  function init() {
     m = murk({
-        dev: true,
-        id: 'demo'
+      dev: true,
+      id: 'demo'
     });
 
-    m.on(['firstExample','secondExample','thirdExample','fourthExample'], function(key) {
-        var count = this.getAttribute('data-murk-count');
-        var el = document.getElementById(key + 'Count');
-        var input = document.getElementById(key);
-        if (!input.value) input.value = decodeURIComponent(this.getAttribute('data-murk-val'));
-        if (count) {
-            m.set(key + 'Count', count);
-            el.style.display = 'inherit';
-        }
-    }).set({
-        firstExample: 'this is',
-        secondExample: 'data binding',
-        thirdExample: 'murked.'
-    });
+    setupSubscribers();
+    setupModel();
+    setupModelOutput();
+    setupBindingEvents();
+  }
 
-    var modelOutput = document.getElementById('model');
+  function setupModelOutput() {
+    modelOutput = document.getElementById('model');
     modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
+  }
 
-    $('[data-murk-example]').on('keyup blur', function(e) {
-        m.set(this.getAttribute('id'), this.value);
-        modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
+  function setupSubscribers() {
+    m.on(['firstExample','secondExample','thirdExample','fourthExample'], function(key) {
+      var count = this.getAttribute('data-murk-count');
+      var el = document.getElementById(key + 'Count');
+      var input = document.getElementById(key);
+      if (!input.value) input.value = decodeURIComponent(this.getAttribute('data-murk-val'));
+      if (count) {
+          m.set(key + 'Count', count);
+          el.style.display = 'inherit';
+      }
     });
-});
+  }
+
+  function setupModel() {
+    m.set({
+      firstExample: 'this is',
+      secondExample: 'data binding',
+      thirdExample: 'murked.'
+    });
+  }
+
+  function setupBindingEvents() {
+    $('[data-murk-example]').on('keyup blur', function(e) {
+      m.set(this.getAttribute('id'), this.value);
+      modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
+    });
+  }
+
+  $(d).ready(init);
+
+})(window,document);
+
