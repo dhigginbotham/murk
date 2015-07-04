3899dac0c9cb3e41bd2c8376dd1f75206acb6cbf
David Higginbotham <davehigginbotham@gmail.com>
2015-07-04 16:53:20 -0600

    Update 2015-07-04T22:53:17.389Z

--- js/example.js
+++ js/example.js
@@ -1,45 +1,29 @@
 (function(w,d) {
-  var m, modelOutput;
+  var modelOutput;
 
   function init() {
-    m = murk({
+    var m = murk({
       dev: true,
       id: 'demo'
     });
-
-    setupSubscribers();
-    setupModel();
-    setupModelOutput();
-    setupBindingEvents();
-  }
-
-  function setupModelOutput() {
-    modelOutput = document.getElementById('model');
-    modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
-  }
-
-  function setupSubscribers() {
     m.on(['firstExample','secondExample','thirdExample','fourthExample'], function(key) {
       var count = this.getAttribute('data-murk-count');
-      var el = document.getElementById(key + 'Count');
-      var input = document.getElementById(key);
-      if (!input.value) input.value = decodeURIComponent(this.getAttribute('data-murk-val'));
+      var el = d.getElementById(key + 'Count');
+      var input = d.getElementById(key);
+      if (!input.value) input.value = m.state.model[key];
       if (count) {
         m.set(key + 'Count', count);
         el.style.display = 'inherit';
       }
-    });
-  }
-
-  function setupModel() {
-    m.set({
+    }).set({
       firstExample: 'this is',
       secondExample: 'data binding',
       thirdExample: 'murked.'
     });
-  }
+    
+    modelOutput = d.getElementById('model');
+    modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
 
-  function setupBindingEvents() {
     $('[data-murk-example]').on('keyup blur', function(e) {
       m.set(this.getAttribute('id'), this.value);
       modelOutput.innerHTML = JSON.stringify({model: m.state.model, keys: m.state.keys},null,2);
