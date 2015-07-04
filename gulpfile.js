92a69620a8d64a7b8c079cc1a03764e1a057847a
David Higginbotham <davehigginbotham@gmail.com>
2015-07-04 03:08:25 -0600

    added gulp stuffs

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+    guglify = require('gulp-uglify'),
+    ggzip = require('gulp-gzip'),
+    gpages = require('gulp-gh-pages'),
+    gutil = require('gulp-util');
+
+gulp.task('min', function() {
+  return gulp.src('./src/murk.js')
+    .on('error', gutil.log)
+    .pipe(guglify())
+    .pipe(gulp.dest('./dist'))
+    .pipe(gulp.dest('./examples/js'));
+});
+
+gulp.task('zip', function() {
+  return gulp.src('./dist/murk.js')
+    .on('error', gutil.log)
+    .pipe(ggzip())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('gh', function() {
+  return gulp.src('./examples/**/*')
+    .pipe(gpages());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./src/**/*', ['min', 'zip']);
+});
+
+gulp.task('default', ['min', 'zip', 'watch']);
